e74d64133d44261391018347994052dfade27546
Radosław Mocarski <rmocarski@softguard.pl>
2024-05-06 12:12:33 +0200

    handler

--- CarRental_Net60/DDD.CarRental.Core/ApplicationLayer/Queries/Handlers/QueryHandler.cs
+++ CarRental_Net60/DDD.CarRental.Core/ApplicationLayer/Queries/Handlers/QueryHandler.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DDD.CarRental.Core.InfrastructureLayer.EF;
+using DDD.CarRental.Core.ApplicationLayer.Mappers;
+using DDD.CarRental.Core.ApplicationLayer.DTOs;
+using DDD.CarRental.Core.DomainModelLayer.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DDD.CarRental.Core.ApplicationLayer.Queries.Handlers
 {
     public class QueryHandler
     {
+        private CarRentalDbContext _dbContext;
+        private Mapper _mapper;
+
+        public QueryHandler(CarRentalDbContext context, Mapper mapper)
+        {
+            _dbContext = context;
+            _mapper = mapper;
+        }
+
+        public List<CarDTO> Execute(GetAllCarsQuery query)
+        {
+            var cars = _dbContext.Cars
+                .AsNoTracking()
+                .ToList();
+
+            List<CarDTO> result = cars.Select(r => this._mapper.Map(r)).ToList();
+            return result;
+        }
     }
 }
