88b72db7274ca8f95dd2284d9c44bca964d88944
Radosław Mocarski <rmocarski@softguard.pl>
2024-05-07 14:12:02 +0200

    operators in distance implemented

--- CarRental_Net60/DDD.CarRental.Core/DomainModelLayer/Models/Distance.cs
+++ CarRental_Net60/DDD.CarRental.Core/DomainModelLayer/Models/Distance.cs
@@ -23,15 +23,135 @@ namespace DDD.CarRental.Core.DomainModelLayer.Models
             switch (DistanceUnit)
             {
                 case Unit.meter:
-                    yield return Value;
+                    yield return Value *1000;
                     break;
                 case Unit.mile:
-                    yield return Value * 1609.344;
+                    yield return Value * 1609344;
                     break;
                 case Unit.kilometer:
-                    yield return Value * 1000;
+                    yield return Value * 1000000;
+                    break;
+                case Unit.centimeter:
+                    yield return Value * 10;
+                    break;
+                case Unit.millimeter:
+                    yield return Value;
+                    break;
+                case Unit.inch:
+                    yield return Value * 25.4;
+                    break;
+                case Unit.foot:
+                    yield return Value * 304.8;
+                    break;
+                case Unit.yard:
+                    yield return Value * 914.4;
                     break;
             }
+        }
+
+        private float CalculateToMM()
+        {
+            switch (DistanceUnit)
+            {
+                case Unit.meter:
+                    return Value * 1000;
+                case Unit.mile:
+                    return Value * 1609344;
+                case Unit.kilometer:
+                    return Value * 1000000;
+                case Unit.centimeter:
+                    return Value * 10;
+                case Unit.millimeter:
+                    return Value;
+                case Unit.inch:
+                    return (float)(Value * 25.4);
+                case Unit.foot:
+                    return (float)(Value * 304.8);
+                case Unit.yard:
+                    return (float)(Value * 914.4);
+            }
+            throw new NotImplementedException();
+        }
+
+        public static Distance operator +(Distance m, Distance m2)
+        {
+            if (!AreCompatibleCurrencies(m, m2))
+            {
+                m.Value = m.CalculateToMM() / 1000000;
+                m.DistanceUnit = Unit.kilometer;
+                m2.Value = m2.CalculateToMM();
+                m2.DistanceUnit = Unit.kilometer;
+            }
+            return new Distance(m.Value + m2.Value, m.DistanceUnit);
+        }
+
+        public static Distance operator -(Distance m, Distance m2)
+        {
+            if (!AreCompatibleCurrencies(m, m2))
+            {
+                m.Value = m.CalculateToMM()/1000000;
+                m.DistanceUnit = Unit.kilometer;
+                m2.Value = m2.CalculateToMM();
+                m2.DistanceUnit = Unit.kilometer;
+            }
+            return new Distance(m.Value - m2.Value, m.DistanceUnit);
+        }
+
+        public Distance MultiplyBy(double multiplier)
+        {
+            return MultiplyBy((float)multiplier);
+        }
+        public Distance MultiplyBy(int multiplier)
+        {
+            return MultiplyBy((float)multiplier);
+        }
+
+        public Distance MultiplyBy(decimal multiplier)
+        {
+            return MultiplyBy((float)multiplier);
+        }
+
+        public Distance MultiplyBy(float multiplier)
+        {
+            return new Distance((Value * multiplier), DistanceUnit);
+        }
+
+        /// <summary>
+        /// Currency is compatible if the same or either money object has zero value.
+        /// </summary>
+        private static bool AreCompatibleCurrencies(Distance m, Distance m2)
+        {
+            return IsZero((decimal)m.Value) || IsZero((decimal)m2.Value) || m.DistanceUnit.Equals(m2.DistanceUnit);
+        }
+
+        private static bool IsZero(decimal testedValue)
+        {
+            return decimal.Zero.CompareTo(testedValue) == 0;
+        }
+
+        public static bool operator <(Distance m, Distance m2)
+        {
+            return m.Value.CompareTo(m2.Value) < 0;
+        }
+
+        public static bool operator >(Distance m, Distance m2)
+        {
+            return m.Value.CompareTo(m2.Value) > 0;
+        }
+
+        public static bool operator >=(Distance m, Distance m2)
+        {
+            return m.Value.CompareTo(m2.Value) >= 0;
+        }
+
+        public static bool operator <=(Distance m, Distance m2)
+        {
+            return m.Value.CompareTo(m2.Value) <= 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}{DistanceUnit}";
         }
     }
 
